c4a3ceb4c7f75421f06fa6bc7b2f4f8ffba3e11b
Anthony <anthonywan00@gmail.com>
2019-08-23 16:41:32 -0400

    added show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'players#index'
-  resources :players, only: [:index, :create]
+  resources :players, only: [:index, :show,:create]
   resources :games, only: [:index, :create]
 end
